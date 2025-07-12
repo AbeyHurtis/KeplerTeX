362261c05985c420c4038e695d839c5d985d247b
Abey12525 <abeyhurtis@gmail.com>
2025-07-11 22:22:25 -0230

    wip: compiled pdf preview

---
+++ src/preview.ts
@@ -0,0 +1,9 @@
+import * as vscode from 'vscode'; 
+
+export function preview() { 
+    // TODO 
+    // Check if cache pdf present, 
+    // Check if there is an update to code. 
+    // Preview pdf. 
+}
+
