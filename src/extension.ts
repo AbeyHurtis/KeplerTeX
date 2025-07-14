47132a621c9f88fed59327494214ddc530e4f499
Abey12525 <abeyhurtis@gmail.com>
2025-07-14 16:32:22 -0230

    wip: sendToServer updated with send and receive to compileURL

--- src/extension.ts
+++ src/extension.ts
@@ -4,13 +4,47 @@ import * as vscode from 'vscode';
 import fetch from 'node-fetch';
 import FormData from 'form-data';
 import { Readable } from 'stream';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 
 // Send raw text to texlive server 
-function sendToServer(texRaw, fileName){
+async function sendToServer(texRaw: string, fileName: string){
 	//TODO
 	try{
 		const form = new FormData();
+
+		const latexStream = Readable.from(texRaw);
+		form.append('tex_file', latexStream, { 
+			filename: fileName || 'manuscript.tex',
+			contentType: 'application/x-tex'
+		});
+
+		const compileURL = process.env.COMPILE_URL;
+		const response = await fetch(compileURL||'', {
+			method: 'POST', 
+			body: form as any, 
+			headers: form.getHeaders()
+		})
+
+		if(!response.ok){
+			const errorText = await response.text(); 
+			vscode.window.showErrorMessage("Failed to Compile",errorText);
+			return; 
+		}
+
+		//read buffer
+		const pdfBuffer = await response.buffer();
+		
+	// 	const tempLocation = vscode.Uri.joinPath(vscode.workspace.workspaceFolders?.[0]?.uri || vscode.Uri.file(require('os').tmpdir()),
+    //   'output.pdf').fsPath;
+
+	  //TODO : 
+	//    Open file within vscode ???
+		
+	}
+	catch(error: any) { 
+		vscode.window.showErrorMessage("Failed to connect: ", error.message);
 	}
 }
 
