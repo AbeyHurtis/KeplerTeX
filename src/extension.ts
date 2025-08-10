4b98731a36313352670d2972a701fccfba2d9b29
Abey12525 <abeyhurtis@gmail.com>
2025-08-10 03:26:24 -0230

    feat/bug fix: added state retention for webview panel, reloads in the same panel without creating new panel

--- src/extension.ts
+++ src/extension.ts
@@ -14,11 +14,19 @@ export function activate(context: vscode.ExtensionContext) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 
+	let initiated = false; 
+
+	//start Inital rendering. 
+	const startRenderCmd = vscode.commands.registerCommand('keplertex.startRender', () => {
+		renderPreview(context); 
+		initiated = true; 
+	})
+
 
 	vscode.workspace.onDidSaveTextDocument((document) => {
 
 
-		if (document.languageId == 'latex' || document.fileName.endsWith('.tex')) {
+		if (initiated && (document.languageId == 'latex' || document.fileName.endsWith('.tex'))){
 			const texRaw = document.getText();
 			// const pdfBufferReturn = sendToServer(texRaw, document.fileName);
 			renderPreview(context); 
--- src/preview.ts
+++ src/preview.ts
@@ -1,84 +1,99 @@
-import * as vscode from 'vscode'; 
-import * as path from 'path'; 
+import * as vscode from 'vscode';
+import * as path from 'path';
 
+
+let panel: vscode.WebviewPanel | undefined;
 
 function getNonce() {
-	let text = '';
-	const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-	for (let i = 0; i < 32; i++) {
-		text += possible.charAt(Math.floor(Math.random() * possible.length));
-	}
-	return text;
+    let text = '';
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (let i = 0; i < 32; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
 }
 
 
-export function renderPreview(context: vscode.ExtensionContext){
-    const panel = vscode.window.createWebviewPanel(
-                    'pdfPreview',
-                    'PDF Preview',
-                    vscode.ViewColumn.Two,
-                    {
-                        enableScripts: true,
-                        localResourceRoots: [
-                            vscode.Uri.joinPath(context.extensionUri, 'lib')
-                        ]
-                    }
-                );
+export function renderPreview(context: vscode.ExtensionContext) {
+    if (!panel) {
+
+        panel = vscode.window.createWebviewPanel(
+            'pdfPreview',
+            'PDF Preview',
+            vscode.ViewColumn.Two,
+            {
+                enableScripts: true,
+                retainContextWhenHidden: true,
+                localResourceRoots: [
+                    vscode.Uri.joinPath(context.extensionUri, 'lib')
+                ]
+            }
+        );
+
+        panel.onDidDispose(() => {
+            panel = undefined;
+        })
+    }
+
 
     const pdfPath = path.join(context.extensionPath, 'lib', 'tm2.pdf');
-			
+
 
     const webview = panel.webview;
-    
-                const pdfJsUri = webview.asWebviewUri(
-                    vscode.Uri.joinPath(context.extensionUri, 'lib/pdfjs', 'pdf.mjs')
-                );
-    
-                const workerUri = webview.asWebviewUri(
-                    vscode.Uri.joinPath(context.extensionUri, 'lib/pdfjs', 'pdf.worker.mjs')
-                );
-    
-                const pdfFileUri = webview.asWebviewUri(
-                    vscode.Uri.file(pdfPath)
-                );
-                
-                const renderScriptUri = webview.asWebviewUri( 
-                    vscode.Uri.joinPath( context.extensionUri, 'lib/renderjs', 'render.js')
-                );
-    
-                // Set webview HTML
-                panel.webview.html = getWebviewHtml(webview, pdfJsUri, workerUri, pdfFileUri, renderScriptUri);
-    
+
+    const pdfJsUri = webview.asWebviewUri(
+        vscode.Uri.joinPath(context.extensionUri, 'lib/pdfjs', 'pdf.mjs')
+    );
+
+    const workerUri = webview.asWebviewUri(
+        vscode.Uri.joinPath(context.extensionUri, 'lib/pdfjs', 'pdf.worker.mjs')
+    );
+
+    const pdfFileUri = webview.asWebviewUri(
+        vscode.Uri.file(pdfPath)
+    );
+
+    const renderScriptUri = webview.asWebviewUri(
+        vscode.Uri.joinPath(context.extensionUri, 'lib/renderjs', 'render.js')
+    );
+
+    const renderCSSUri = webview.asWebviewUri(
+        vscode.Uri.joinPath(context.extensionUri, 'lib/renderjs', 'render.css')
+    );
+
+    // Set webview HTML
+    webview.html = getWebviewHtml(webview, pdfJsUri, workerUri, pdfFileUri, renderScriptUri, renderCSSUri);
+
 }
 
 
 function getWebviewHtml(
-	webview: vscode.Webview,
-	pdfJsUri: vscode.Uri,
-	workerUri: vscode.Uri,
-	pdfFileUri: vscode.Uri,
-    renderUri: vscode.Uri
+    webview: vscode.Webview,
+    pdfJsUri: vscode.Uri,
+    workerUri: vscode.Uri,
+    pdfFileUri: vscode.Uri,
+    renderUri: vscode.Uri,
+    renderCSSUri: vscode.Uri
 ) {
-	const nonce = getNonce();
+    const nonce = getNonce();
 
-	return `
+    return `
     <!DOCTYPE html>
     <html>
     <head>
-      <meta charset="UTF-8">
-      <meta http-equiv="Content-Security-Policy"
-		content="default-src 'none';
-		connect-src ${webview.cspSource};
-		script-src 'unsafe-inline' ${webview.cspSource};
-		worker-src blob:;
-		style-src ${webview.cspSource} 'nonce-${nonce}';
-		img-src ${webview.cspSource};
-		font-src ${webview.cspSource};
-		">
+        <meta charset="UTF-8">
+        <meta http-equiv="Content-Security-Policy"
+            content="default-src 'none';
+            connect-src ${webview.cspSource};
+            script-src 'unsafe-inline' ${webview.cspSource};
+            worker-src blob:;
+            style-src ${webview.cspSource} 'nonce-${nonce}';
+            img-src ${webview.cspSource};
+            font-src ${webview.cspSource};
+            ">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-	<style nonce="${nonce}">
-	body { margin: 0; padding: 0; }
-	canvas { display: block; margin: auto; }
+	<style nonce="${nonce}" src='${renderCSSUri}'>
 	</style>
     </head>
     <body>
